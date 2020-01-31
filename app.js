9a51748689dff71f31dd838f8004d6767fc13558
Obradovic Nikola <obradovicnikola009@gmail.com>
2020-01-31 13:21:26 +0100

    Initial commit

--- app.js
+++ app.js
@@ -4,62 +4,62 @@ const notes = require('./notes')
 yargs.version('1.1.0')
 
 yargs.command({
-    command: "add",
-    describe: "Adding a new note",
-    builder: {
-        title: {
-            describe: "Note title",
-            demandOption: true,
-            type: 'string'
-        },
-        body: {
-            describe: "Note text",
-            demandOption: true,
-            type: 'string'
-        }
-    },
-    handler: function (argv) {
-        notes.addNote(argv.title, argv.body)
-    }
+	command: "add",
+	describe: "Adding a new note",
+	builder: {
+		title: {
+			describe: "Note title",
+			demandOption: true,
+			type: 'string'
+		},
+		body: {
+			describe: "Note text",
+			demandOption: true,
+			type: 'string'
+		}
+	},
+	handler: function (argv) {
+		notes.addNote(argv.title, argv.body)
+	}
 })
 
 yargs.command({
-    command: "remove",
-    describe: "Removing a new note",
-    builder: {
-        title: {
-            describe: "Note title",
-            demandOption: true,
-            type: 'string'
-        }
-    },
-    handler: function (argv) {
-        notes.removeNote(argv.title)
-    }
+	command: "remove",
+	describe: "Removing a new note",
+	builder: {
+		title: {
+			describe: "Note title",
+			demandOption: true,
+			type: 'string'
+		}
+	},
+	handler: function (argv) {
+		notes.removeNote(argv.title)
+	}
 })
 
 yargs.command({
-    command: "list",
-    describe: "Listing notes",
-    handler: function () {
-        notes.listNotes();
-    }
+	command: "list",
+	describe: "Listing notes",
+	handler: function () {
+		notes.listNotes();
+	}
 })
 
 yargs.command({
-    command: "read",
-    describe: "Reading a note",
-    builder: {
-        title: {
-            describe: "Note title",
-            demandOption: true,
-            type: 'string'
-        }
-    },
-    handler: function (argv) {
-        notes.readNote(argv.title)
-    }
+	command: "read",
+	describe: "Reading a note",
+	builder: {
+		title: {
+			describe: "Note title",
+			demandOption: true,
+			type: 'string'
+		}
+	},
+	handler: function (argv) {
+		notes.readNote(argv.title)
+	}
 })
 
-// console.log(yargs.argv);
+console.log(yargs.argv);
 yargs.parse()
